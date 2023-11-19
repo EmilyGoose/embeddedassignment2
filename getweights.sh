e455f0c0ba446001aa6c8b24702d32c600643d5f
Emily Goose <13226962+EmilyGoose@users.noreply.github.com>
2023-11-19 14:00:29 -0500

    Add files via upload

---
+++ getweights.sh
@@ -0,0 +1,4 @@
+mkdir yolo && cd yolo
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+wget https://raw.githubusercontent.com/pjreddie/darknet/f6afaabcdf85f77e7aff2ec55c020c0e297c77f9/data/coco.names
---
+++ start.sh
@@ -0,0 +1,2 @@
+python3 detect.py --input fortnite.mp4 --output output.avi --yolo yolo
+
